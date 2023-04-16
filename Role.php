9668f2d96e9bf70f9fc276da24bf73084c0ace91
ValHDL <112008036+ValHDL@users.noreply.github.com>
2023-04-16 15:22:14 +0200

    Add files via upload

---
+++ Role.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Db\DbConnection;
+
+require_once "../model/Connection.php";
+
+class Role
+{
+    #region//Propriétés
+    public int $idRole = -1;
+    public string $libRole;
+    #endregion
+
+    #region//Constructeur
+    public function __construct(string $libRole = '')
+    {
+        $this->libRole = $libRole;
+    }
+    #endregion
+
+    #region//Méthodes
+
+    public function getRoleByUtilisateur($idUtilisateur)
+    {
+        $db = DbConnection::getInstance();
+        $stmt = $db->prepare("SELECT * FROM posseder P INNER JOIN role R on R.idRole = P.idRole WHERE idUtilisateur = :idUtilisateur");
+        $stmt->bindParam(":idUtilisateur", $idUtilisateur);
+        $stmt->execute();
+        $roleByUtilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
+        $db->close();
+        return $roleByUtilisateur;
+    }
+
+    public function addRole()
+    {
+        $db = DbConnection::getInstance();
+        $stmt = $db->prepare("INSERT INTO role (libRole) VALUES (:libRole)");
+        $stmt->bindParam(":libRole", $this->libRole);
+        $stmt->execute();
+        $db->close();
+    }
+    #endregion
+}
