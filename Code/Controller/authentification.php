0db13345c6391893e1c395794e73c753b91890e2
ValHDL <valerie.houdaille@hinodev.com>
2023-05-05 16:23:34 +0200

    login

--- Code/Controller/authentification.php
+++ Code/Controller/authentification.php
@@ -4,6 +4,4 @@
 session_start();
 if (!isset($_SESSION['user'])) {
     header('Location: ../controller/login.php');
-    $_SESSION['user_idUtilisateur'] = $_SESSION['user']['idUtilisateur'];
-    $_SESSION['user_idRole'] = $_SESSION['user']['idRole'];
 }
