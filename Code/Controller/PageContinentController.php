80511df352b904fe2eb624598cf40b61ddff176d
JNC-Hub <62072327+JNC-Hub@users.noreply.github.com>
2023-05-05 09:29:49 +0200

    ajout controller affichage image, ajout requete recette, modification page continent

---
+++ Code/Controller/PageContinentController.php
@@ -0,0 +1,15 @@
+<?php
+require_once "../model/Connection.php";
+require_once "../model/Recette.php";
+
+// ID de la recette à afficher
+$idRecette = 5;
+
+$imageRecette = new Recette();
+$imageRecette->image = $imageRecette->getImageById($idRecette);
+
+echo '<img src="http://projetcubes3/'. $imageRecette->image.'" width="300" height="200" />';
+
+
+
+
