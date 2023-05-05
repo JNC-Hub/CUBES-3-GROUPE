199eadf1cdc31614382bc60bec09f99ed91d91da
ValHDL <valerie.houdaille@hinodev.com>
2023-05-05 10:01:15 +0200

    login

--- Code/Controller/authentification.php
+++ Code/Controller/authentification.php
@@ -1,10 +1,9 @@
 <?php
 
 //Vérifie que l'utilisateur est bien connecté
-session_start(); // Démarre la session
-if (!isset($_SESSION['user'])) { // Vérifie si la variable de session 'user' est définie
-    header('Location: ../controller/login.php'); // Redirige vers la page de connexion si l'utilisateur n'est pas connecté
-} else {
-    $_SESSION['user_idUtilisateur'] = intval($_SESSION['user']['idUtilisateur']);
-    $_SESSION['user_idRole'] = intval($_SESSION['user']['idRole']);
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('Location: ../controller/login.php');
+    $_SESSION['user_idUtilisateur'] = $_SESSION['user']['idUtilisateur'];
+    $_SESSION['user_idRole'] = $_SESSION['user']['idRole'];
 }
