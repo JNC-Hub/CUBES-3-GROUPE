bd7d46190296fc883e3d743d3086aa4cd3606842
JNC-Hub <62072327+JNC-Hub@users.noreply.github.com>
2023-05-29 16:02:39 +0200

    Modification controller affichageimagecontinent
    Modification du nom pour respecter les convention et suppression du code superflu

--- Code/Controller/affichageImageRecetteContinent.php
+++ Code/Controller/affichageImageRecetteContinent.php
@@ -2,27 +2,6 @@
 require_once "../model/Connection.php";
 require_once "../model/Recette.php";
 
-// // ID de la recette à afficher
-// $idRecette = 2;
-
-// // Vérifier si l'ID de la recette est présent dans la requête
-// if (isset($_GET['idRecette'])) {
-//     // Récupérer l'ID de la recette depuis la requête
-//     $idRecette = $_GET['idRecette'];
-
-//     $imageRecette = new Recette();
-//     $imageRecette->image = $imageRecette->getImageById($idRecette);
-
-//     if ($imageRecette->image !== null) {
-//         // Afficher l'image
-//         echo '<img src="../imageRecipe/' . $imageRecette->image . '" width="300" height="200" />';
-//     } else {
-//         echo "Aucune image trouvée pour cette recette";
-//     }
-// } else {
-//     echo "ID de recette manquant dans la requête";
-// }
-
 // Créer une instance de Recette
 $recette = new Recette();
 
