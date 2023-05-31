d6a64be1691398ee67a983b2bfd81e8e1ec8b286
JNC-Hub <62072327+JNC-Hub@users.noreply.github.com>
2023-05-31 16:53:05 +0200

    modification simple
    Modif nom de fichier image, modification lien header, décommenter le controlerdetailrecette

--- Code/Controller/detailRecette.php
+++ Code/Controller/detailRecette.php
@@ -3,23 +3,24 @@ require_once '../Model/Recette.php';
 require_once '../Model/Etape.php';
 require_once '../Model/Contenir.php';
 
-//if (isset($_GET['idRecette'])) {
+if (isset($_GET['idRecette'])) {
 
-$idRecette = 3;
-// $idRecette = $_GET['idRecette'];
+    //$idRecette = 3;
+    $idRecette = $_GET['idRecette'];
 
-$recette = new Recette();
-$recette = $recette->getRecipe($idRecette);
+    $recette = new Recette();
+    $recette = $recette->getRecipe($idRecette);
 
-//Image de la recette
-$imageFile = glob('../imageRecipe/' . $idRecette . '.*');
-$image = $imageFile[0];
+    //Image de la recette
+    $imageFile = glob('../imageRecipe/' . $idRecette . '.*');
+    $image = $imageFile[0];
 
-$contenirIngredients = new Contenir();
-$ingredients = $contenirIngredients->getIngredientsRecipe($idRecette);
 
-$etapes = new Etape();
-$etapes = $etapes->getEtapesRecipe($idRecette);
+    $contenirIngredients = new Contenir();
+    $ingredients = $contenirIngredients->getIngredientsRecipe($idRecette);
 
-require_once '../View/detailRecette.php';
-//}
+    $etapes = new Etape();
+    $etapes = $etapes->getEtapesRecipe($idRecette);
+
+    require_once '../View/detailRecette.php';
+}
