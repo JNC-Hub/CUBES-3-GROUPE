729a5028c8dc2f3dc8da17a152e068fa80f53432
ValHDL <valerie.houdaille@hinodev.com>
2023-06-05 16:20:55 +0200

    controllers

--- Code/Controller/compteUtilisateur.php
+++ Code/Controller/compteUtilisateur.php
@@ -1,5 +1,15 @@
 <?php
-require_once 'authentification.php';
+require_once '../Controller/authentification.php';
 require_once '../Model/Utilisateur.php';
+
+require_once("../Model/Recette.php");
+require_once("../Model/Continent.php");
+require_once("../Model/Pays.php");
+require_once("../Model/StatutRecette.php");
+$recette = new Recette();
+if (isset($_SESSION['user'])) {
+    $idUtilisateur = $_SESSION['user']['idUtilisateur'];
+}
+$listRecipesUser = $recette->getRecipesUser(intval($idUtilisateur));
 
 require_once '../View/compteUtilisateur.php';
