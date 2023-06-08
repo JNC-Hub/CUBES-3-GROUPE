7721a36866e241289ac8c474fbcd84ddbead0f1a
JNC-Hub <62072327+JNC-Hub@users.noreply.github.com>
2023-06-08 13:55:51 +0200

    [Feature] Image acceuil
    modification controller AffichageimageRecetteUnique  pour afficher une seul image en fonction de la derniere id enregistrée.
    ajout d'une méthode get recettesbyID dans model recette et modification de la vue acceuil pour prendre en compte le controleur imageunique et le css des étoiles

--- Code/Controller/affichageImageRecetteUnique.php
+++ Code/Controller/affichageImageRecetteUnique.php
@@ -1,69 +1,54 @@
 <?php
-require_once "../model/Connection.php";
-require_once "../model/Recette.php";
-require_once "../model/Continent.php";
-require_once "../model/Pays.php";
+require_once "../Model/Connection.php";
+require_once "../Model/Recette.php";
+require_once "../Model/Pays.php";
 require_once "../Model/Note.php";
 
 // Créer une instance de Recette
 $recette = new Recette();
 $pays = new Pays();
-$continent = new Continent();
 $note = new Note();
 
-// Vérifier si l'ID du continent est présent dans la requête
-if (isset($_GET['idContinent'])) {
-    // Récupérer l'ID du continent depuis la requête
-    $idContinent = $_GET['idContinent'];
+// Récupérer toutes les recettes triées par date de publication décroissante
+$recettesValidees = $recette->getRecettesById();
 
-    $recettesValidees = $recette->getRecetteByContinent($idContinent);
-}
+// Obtenir la dernière recette publiée (la première du tableau)
+$recetteValidee = reset($recettesValidees);
 
-// Calculer le nombre de recettes validées
-$nombreRecettes = count($recettesValidees);
+// Vérifier si une recette est disponible
+if ($recetteValidee) {
+    $idRecette = $recetteValidee['idRecette'];
+    $idPays = $recetteValidee['idPays'];
+    $titreRecette = $recetteValidee['titre'];
 
-// Calculer le nombre de lignes nécessaires
-$nombreLignes = ceil($nombreRecettes / 3);
+    // Récupérer le nom du pays
+    $nomPays = $pays->getLibPays($idPays);
 
-// Diviser le tableau de recettes validées en sous-tableaux de 3 recettes par ligne
-$lignesRecettesValidees = array_chunk($recettesValidees, 3);
+    // Récupérer la note de la recette
+    $averageNote = $note->getNoteRecette($idRecette);
 
-// Afficher les lignes et les vignettes
-foreach ($lignesRecettesValidees as $ligne) {
     echo '<div class="row">';
+    echo '<div class="col-md-4">';
+    echo '<a href="../Controller/detailRecette.php?idRecette=' . $idRecette . '">';
+    echo '<img src="../imageRecipe/' . $idRecette . '" alt="' . $titreRecette . '" width="300" height="200" />';
+    echo '<h4>' . $titreRecette . '</h4>';
+    echo '<p> ' . $nomPays . '</p>';
 
-    foreach ($ligne as $recetteValidee) {
-        $idRecette = $recetteValidee['idRecette'];
-        $idPays = $recetteValidee['idPays'];
-        $titreRecette = $recetteValidee['titre'];
-
-        // Récupérer le nom du pays
-        $nomPays = $pays->getLibPays($idPays);
-
-        // Récupérer la note de la recette
-        $averageNote = $note->getNoteRecette($idRecette);
-
-        echo '<div class="col-md-4">';
-        echo '<a href="../Controller/detailRecette.php?idRecette=' . $idRecette . '">';
-        echo '<img src="../imageRecipe/' . $idRecette . '" alt="' . $titreRecette . '" width="300" height="200" />';
-        echo '<h4>' . $titreRecette . '</h4>';
-        echo '<p> ' . $nomPays . '</p>';
-
-        // Afficher les étoiles en fonction de la note
-        $starRating = '';
-        for ($i = 1; $i <= 5; $i++) {
-            if ($i <= round($averageNote)) {
-                $starRating .= '<i class="fas fa-star"></i>'; // Étoile pleine
-            } else {
-                $starRating .= '<i class="far fa-star"></i>'; // Étoile vide
-            }
+    // Afficher les étoiles en fonction de la note
+    $starRating = '';
+    for ($i = 1; $i <= 5; $i++) {
+        if ($i <= round($averageNote)) {
+            $starRating .= '<i class="fas fa-star"></i>'; // Étoile pleine
+        } else {
+            $starRating .= '<i class="far fa-star"></i>'; // Étoile vide
         }
-        echo '<p>Note: <span class="star-rating">' . $starRating . '</span></p>';
-
-        // echo '<p>Note: ' . $averageNote . '</p>';
-        echo '</a>';
-        echo '</div>';
     }
+    echo '<p>Note: <span class="star-rating">' . $starRating . '</span></p>';
 
+    // echo '<p>Note: ' . $averageNote . '</p>';
+    echo '</a>';
     echo '</div>';
+    echo '</div>';
+} else {
+    echo 'Aucune recette publiée pour le moment.';
 }
