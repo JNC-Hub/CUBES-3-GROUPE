beb9ce115bb50380edb40babd50b9f23c99089a3
JNC-Hub <62072327+JNC-Hub@users.noreply.github.com>
2023-06-17 17:13:58 +0200

    [Update] FrontAPPmob
    Suppression du drawer et ajout d'une flèche de retour arriere vers l'acceuil.
    Modification du css de la page affichageUtilisateur

--- Code/portal_app/lib/affichageUtilisateur.dart
+++ Code/portal_app/lib/affichageUtilisateur.dart
@@ -10,6 +10,12 @@ class AffichageUtilisateurPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
         title: Text(
           'Les Voyageurs Gourmands',
           style: TextStyle(
@@ -21,34 +27,59 @@ class AffichageUtilisateurPage extends StatelessWidget {
           color: Colors.black,
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'ressources/images/logoSite.png',
-              width: 400,
-              height: 400,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('ressources/images/logoSite.png'),
+            fit: BoxFit.fitWidth,
+            colorFilter: ColorFilter.mode(
+              Colors.white.withOpacity(0.2),
+              BlendMode.dstATop,
             ),
-            SizedBox(height: 20),
-            Text(
-              'Bienvenue ${utilisateur.prenom} ${utilisateur.nom} !',
-              style: TextStyle(fontSize: 24),
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 40),
+                Container(
+                  width: 200,
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: Text(
+                    'Bienvenue ${utilisateur.prenom} ${utilisateur.nom} !',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Votre email est : ${utilisateur.mail}',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Return to the home page and remove all navigation history
+                    Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+                  },
+                  child: Text('Se déconnecter'),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            Text(
-              'Votre email est : ${utilisateur.mail}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Retour vers accueil et suppression données de navigation
-                Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
-              },
-              child: Text('Se déconnecter'),
-            ),
-          ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 50,
+        color: Color.fromRGBO(242, 242, 242, 1.0),
+        child: Center(
+          child: Text(
+            '© Droits d\'auteur',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
